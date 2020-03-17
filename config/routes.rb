7d1363afdc009b1c4610db6466d0e74fa16dd159
EC2 Default User <ec2-user@ip-172-31-41-188.ec2.internal>
2020-03-17 23:51:06 +0000

    PG for production

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :users
   
-  root "users#new"
+  root "users#newsudo yum install postgresql-devel"
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
